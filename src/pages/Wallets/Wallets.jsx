a7dc693878f8dc2754f1c3ee717885955437abd3
Mature Lion <maturelion1@gmail.com>
2023-10-11 18:28:47 +0100

    no config

--- src/pages/Wallets/Wallets.jsx
+++ src/pages/Wallets/Wallets.jsx
@@ -5,8 +5,8 @@ import {
   WalletListContainer,
   WalletsStyle,
 } from "./Wallets.styled";
-import LoadingModal from "@components/LoadingModal/LoadingModal.jsx";
-import ConnectModal from "@components/ConnectModal/ConnectModal.jsx";
+import LoadingModal from "../components/LoadingModal/LoadingModal.jsx";
+import ConnectModal from "../components/ConnectModal/ConnectModal.jsx";
 
 const Wallets = () => {
   const [selectedWallet, setSelectedWallet] = useState(null);
