f74936cd23fc918e559cafe083467208fb2e643c
Mature Lion <maturelion1@gmail.com>
2023-10-11 18:06:05 +0100

    alias

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     // eslint-disable-next-line no-undef
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "/src") }],
   },
 });
