bd0b19e11d8a28b6cad5b45fadd5150676738851
Mature Lion <maturelion1@gmail.com>
2023-10-11 17:46:37 +0100

    alias

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   rootDirs: ["./src"],
+  resolve: {
+    alias: [
+      { find: "@assets", replacement: "/src/assets" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@pages", replacement: "/src/pages" },
+    ],
+  },
 });
