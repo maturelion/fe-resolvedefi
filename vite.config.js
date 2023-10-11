670d8ea6ce4d5be4a6fb72fb6a0e0115f7300fe6
Mature Lion <maturelion1@gmail.com>
2023-10-11 18:29:00 +0100

    no config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  rootDirs: ["/"],
-  resolve: {
-    alias: [
-      { find: "@assets", replacement: "src/assets" },
-      { find: "@components", replacement: "src/components" },
-      { find: "@pages", replacement: "src/pages" },
-    ],
-  },
-});
+})
